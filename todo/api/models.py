022397db6486c31323a99c5f0af70ebe432f5167
elprinced <slimprince.sp@gmail.com>
2024-04-25 12:57:35 +0000

    This is the first commit

--- todo/api/models.py
+++ todo/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 
+
 class Todo(models.Model):
     title= models.CharField(blank=False, max_length=50)
     description = models.CharField(max_length=100)
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    account
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
