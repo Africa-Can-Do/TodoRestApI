6fae61f35035a633a8caba7b5cc75f4189ac238e
Eu Gene <wise@masterbuilder.org>
2024-05-04 05:54:35 +0000

    added view and url for account registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+class RegisterView(FormView):
+    form_class = CustomUserCreationForm
+    template_name = 'register.html'
+    success_url = '/home'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        # Prepare the response data
+        response_data = {
+            'email': user.email,
+            'firstname': user.firstname,
+            'lastname': user.lastname,
+            'phone': user.phone,
+            'department': user.department.name, # Assuming department is a ForeignKey
+            'username': user.username,
+        }
+        return JsonResponse(response_data, status=201)
