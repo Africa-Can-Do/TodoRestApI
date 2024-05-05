efb65013d726641ded78107c91cbd5022c3b68eb
Eu Gene <wise@masterbuilder.org>
2024-05-05 15:45:18 +0000

    added JWT authentication

--- project_dir/settings.py
+++ project_dir/settings.py
@@ -137,4 +137,13 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated", 
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
+    ]
+}
+
+REST_AUTH = {
+    "USE_JWT": True,
+    "JWT_AUTH_COOKIE": "todo-auth",
+    "JWT_AUTH_REFRESH_COOKIE": "refresh-token",
 }
